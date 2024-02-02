1271d19d4081d1a3c442f07d02fb7ebf786d58ff
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-02-02 10:15:44 -0500

    2/2

---
+++ vite-project/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 v-if="answer === true">Correct</h1>
+    <h1 v-else>Incorrect</h1>
+    <h3 v-for="animal in animals" :key="animal">{{ animal }}</h3> 
+  </div>
+</template>
+
+
+<script setup> 
+const answer = true;
+const animals = ["Ape", "Zebra", "Dog"]
+</script>
+
+
+<style scoped> 
+
+</style>
