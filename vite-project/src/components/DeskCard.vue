de26aee975683f4b854a5db0d4d0bc845bb2c8f0
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-02-05 10:14:06 -0500

    2/5

---
+++ vite-project/src/components/DeskCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>{{ Destination.name }}</h2>
+        <img :src="Destination.img" alt="">
+        <h3>{{ clicked }}</h3>
+        <button @clicked="increment">Click Me</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props = defineProps({
+    //define data expected to recieve
+    //age: Number or name: String,
+    Destination: Object,
+});
+
+//clicker logic
+const clicked = ref(0);
+function increment(){
+    clicked.value++;
+}
+</script>
+
+<style scoped>
+img {
+    width: 250px;
+    height: 300px; 
+    object-fit: cover;
+}
+</style>
