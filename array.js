39cc9e2ef36dff218d4314821a7ca9a7a236994a
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-02-09 10:13:54 -0500

    2/9

---
+++ array.js
@@ -0,0 +1,74 @@
+const cake = [
+    {
+        name: "White Christmas",
+        info: "Basic vanilla cake with vanilla buttercream.",
+        price: "$60.00",
+        image: "",
+    },
+    {
+        name: "C & C",
+        info: "Chocolate cake with cookie crumble and chocolate glaze.",
+        price: "$72.00",
+        image: "",
+    },
+    {
+        name: "Sweet Sour",
+        info: "Vanilla cake with lemon filling, covered with raspberry buttercream.",
+        price: "$88.00",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+    {
+        name: "",
+        info: "",
+        price: "",
+        image: "",
+    },
+]
