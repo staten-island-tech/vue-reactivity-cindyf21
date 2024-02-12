b1b4ce2b8f709a2a9ef09b5a9b71561fa2313481
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-02-12 10:14:41 -0500

    2/12

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -11,43 +11,43 @@
 <script setup>
 import DeskCard from "@/components/DeskCard.vue";
 const destinations = [
-        {
-          name: "Venetian Renaissance",
-          International: true,
-          price: 1300,
-          img: "https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80&quot;",
-        },
-        {
-          name: "Swiss Mountain Getaway",
-          International: true,
-          price: 800,
-          img: "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80&quot;",
-        },
-        {
-          name: "Ibizan Coastal Vacation",
-          International: true,
-          price: 1200,
-          img: "https://images.unsplash.com/photo-1547138494-97041dec734b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80&quot;",
-        },
-        {
-          name: "Hawaiian Vacation",
-          International: false,
-          price: 900,
-          img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80&quot;",
-        },
-        {
-          name: "Cascade Mountains",
-          International: false,
-          price: 400,
-          img: "https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80&quot;",
-        },
-        {
-          name: "Egyptian Exploration",
-          International: true,
-          price: 800,
-          img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
-        },
-      ]
+{
+        name: "Happy Fairy Pink Unicorn",
+        flavor: "Strawberry",
+        price: "$88.00",
+        image: "/pinkcake.jpg",
+    },
+    {
+        name: "Death Wednesday Goth Black",
+        flavor: "Blackberry",
+        price: "$88.00",
+        image: "/blackcake.jpg",
+    },
+    {
+        name: "Cream Milkshake White Ice",
+        flavor: "Pineberry",
+        price: "$88.00",
+        image: "/whitecake.jpg",
+    },
+    {
+        name: "Fresh Mint Air Water",
+        flavor: "Gooseberry",
+        price: "$88.00",
+        image: "/mintcake.jpg",
+    },
+    {
+        name: "Happy Birthday Pink Fairy",
+        flavor: "Raspberry",
+        price: "$60.00",
+        image: "./public/pinkbday.jpg",
+    },
+    {
+        name: "Happy Brithday Ocean Blue",
+        flavor: "Blueberry",
+        price: "$60.00",
+        image: "./public/bluebday.jpg",
+    }
+  ]
 </script>
 
 <style scoped> </style>
